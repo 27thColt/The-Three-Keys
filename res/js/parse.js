5bf9116746d6a45bc642194aa4cebc9d967ce942
27thColt <d27thColt@gmail.com>
2014-05-20 18:42:53 +0800

    Reads the commands and sees if it is a command
    Basically it checks for a certain part of the command and checks if it
    is a command or not. If not it goes to check the next condition.

---
+++ res/js/parse.js
@@ -0,0 +1,80 @@
+//for parsing the text
+parse = function(string) {
+	//help command
+	if (string.substr(0, 4) == "help") {
+		$("#game").empty();
+		$("#game").append("Commands: <br> -look [looks at the current area the player is in] <br> -go (north/east/south/west) [go to another area] <br> -examine (item) [looks at an item] <br> -get (item) [puts an item from your current area and puts it in you inventory for later use] <br> -inventory [give you a list of items in your inventory]");
+		$("#game").append("<br> -use (item) [use an item that is in your inventory]");
+		$("#game").append("<br> <br> type \"look\" to see your surroundings");
+
+	//look command
+	} else if (string.substr(0, 4) == "look") {
+		player.room.display();
+
+	//go command
+	} else if (string.substr(0, 2) == "go") {
+
+		//checks which direction the player wants to go
+		if (string.substr(3, 5) == "north") {
+			if (player.room.exits.NORTH != null) {
+				player.changeRoom(player.room, player.room.exits.NORTH);
+				player.room.display();
+			} else {
+				$("#game").append("<br>You can't go that way.");
+			};
+		} else if (string.substr(3, 5) == "south") {
+			if (player.room.exits.SOUTH != null) {
+				player.changeRoom(player.room, player.room.exits.SOUTH);
+				player.room.display();
+			} else {
+				$("#game").append("<br>You can't go that way.");
+			};
+		} else if (string.substr(3, 4) == "east") {
+			if (player.room.exits.EAST != null) {
+				player.changeRoom(player.room, player.room.exits.EAST);
+				player.room.display();
+			} else {
+				$("#game").append("<br>You can't go that way.");
+			};
+		} else if (string.substr(3, 4) == "west") {
+			if (player.room.exits.WEST != null) {
+				player.changeRoom(player.room, player.room.exits.WEST);
+				player.room.display();
+			} else {
+				$("#game").append("<br>You can't go that way.");
+			};
+		};
+
+	//examine commmand
+	} else if (string.substr(0, 7) == "examine") {
+		//test for the room 
+		for (var i = 0; i < player.room.items.length; i++) {
+			if (string.substr(8, string.length - 8) == player.room.items[i].name) {
+				player.room.items[i].display();
+				break;
+			};
+		};
+		//if that doesn't work, try the inventory
+		for (var i = 0; i < player.inventory.length; i++) {
+			if (string.substr(8, string.length - 8) == player.inventory[i].name) {
+				player.inventory[i].display();
+				break;
+			};
+		};
+
+	//the get command
+	} else if (string.substr(0,3) == "get") {
+		player.getItem(string.substr(4, string.length - 4));
+
+	//use command
+	} else if (string.substr(0, 3) == "use"){
+		for (var i = 0; i < player.inventory.length; i++) {
+			if (string.substr(4, string.length - 4) == player.inventory[i].name && player.inventory[i].usable == true) {
+				player.inventory[i].useFunc();
+			};
+		};
+	} else {
+		$("#game").empty();
+		$("#game").append("Sorry, \"" + input +  "\" is not a command" + "<br> type \"help\" for a list of commands.");
+	};
+};
